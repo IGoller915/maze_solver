b32454f2326687ebd52bc8e4bd988739e188a559
Ian Goller <ian.goller915@gmail.com>
2025-04-18 06:54:20 -0400

    add windown close method and main.py

--- window.py
+++ window.py
@@ -4,17 +4,22 @@ class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.root = Tk()
-        self.root.title("Maze Solver")
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.geometry(f"{width}x{height}")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas()
         self.canvas.pack()
         self.running = False
 
     def redraw(self):
-        self.root.update()
-        self.root.update_idletasks()
+        self.__root.update()
+        self.__root.update_idletasks()
 
     def wait_for_close(self):
         self.running = True
         while self.running == True:
             self.redraw()
+
+    def close(self):
+        self.running = False
